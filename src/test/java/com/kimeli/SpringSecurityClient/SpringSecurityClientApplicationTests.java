364b6ec0ec584caa26b0226e187b57325dc65fb9
Kimeli <jobkimeli2016@gmail.com>
2022-10-28 19:45:38 +0300

    Initial commit

---
+++ src/test/java/com/kimeli/SpringSecurityClient/SpringSecurityClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimeli.SpringSecurityClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
